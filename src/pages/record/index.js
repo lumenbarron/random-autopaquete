86a8ef5c79451d67230e2312ade735141bf0ceec
Brayan Garcia Cuellar <bgarciacuellar@outlook.com>
2020-05-22 17:31:34 -0500

    overweight css test

--- src/pages/record/index.js
+++ src/pages/record/index.js
@@ -3,6 +3,7 @@ import { Column, Badge, TableWithBrowserPagination, Input } from 'react-rainbow-
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { StyledRecord } from './styled';
 
 const StyledBadge = styled(Badge)`
     color: #09d3ac;
@@ -13,38 +14,42 @@ const containerTableStyles = { height: 256 };
 
 const RecordPage = () => {
     return (
-        <div>
-            <div>
-                <h1>Mis envíos</h1>
+        <StyledRecord>
+            <div className="trecord">
+                <div className="boxtable">
+                    <h1>Mis envíos</h1>
 
-                <div className="rainbow-align-content_center rainbow-p-vertical_x-large rainbow-flex_wrap">
-                    <Input
-                        className="rainbow-p-around_medium"
-                        placeholder="Buscar"
-                        icon={<FontAwesomeIcon icon={faSearch} className="rainbow-color_gray-3" />}
-                    />
-                </div>
+                    <div className="rainbow-align-content_center rainbow-p-vertical_x-large rainbow-flex_wrap">
+                        <Input
+                            className="rainbow-p-around_medium"
+                            placeholder="Buscar"
+                            icon={
+                                <FontAwesomeIcon icon={faSearch} className="rainbow-color_gray-3" />
+                            }
+                        />
+                    </div>
 
-                <div className="rainbow-p-bottom_xx-large">
-                    <div style={containerStyles}>
-                        <TableWithBrowserPagination
-                            pageSize={10}
-                            keyField="id"
-                            style={containerTableStyles}
-                        >
-                            <Column header="Fecha " field="date" />
-                            <Column header="Guía" field="guide" />
-                            <Column header="Origen" field="origin" />
-                            <Column header="Destino" field="Destination" />
-                            <Column header="Peso" field="weight" />
-                            <Column header="Servicio" field="service" />
-                            <Column header="Status" field="status" component={StatusBadge} />
-                            <Column header="Costo" field="cost" />
-                        </TableWithBrowserPagination>
+                    <div className="rainbow-p-bottom_xx-large">
+                        <div style={containerStyles}>
+                            <TableWithBrowserPagination
+                                pageSize={10}
+                                keyField="id"
+                                style={containerTableStyles}
+                            >
+                                <Column header="Fecha " field="date" />
+                                <Column header="Guía" field="guide" />
+                                <Column header="Origen" field="origin" />
+                                <Column header="Destino" field="Destination" />
+                                <Column header="Peso" field="weight" />
+                                <Column header="Servicio" field="service" />
+                                <Column header="Status" field="status" component={StatusBadge} />
+                                <Column header="Costo" field="cost" />
+                            </TableWithBrowserPagination>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </StyledRecord>
     );
 };
 
--- src/pages/record/styled.js
+++ src/pages/record/styled.js
@@ -1,1 +1,14 @@
 import styled from 'styled-components';
+
+export const StyledRecord = styled.div`
+    .trecord {
+        background: #f7ece2;
+        padding: 40px 20px 20px 20px;
+    }
+
+    .boxtable {
+        box-shadow: 10px;
+        background: #fff;
+        border-radius: 45px;
+    }
+`;
