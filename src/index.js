31a97d9bac56e58f6ff74f90c62a30a73fa63c17
Alammduran <alam_duran@hotmail.com>
2020-05-23 00:15:45 -0500

    Agregue la api y los parámetros de firebase como también agregarlo en el index para que se pueda usar en todo el proyecto

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+import { FirebaseAppProvider } from 'reactfire';
 import App from './app';
+import firebase from './components/firebase/firebase';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <FirebaseAppProvider firebaseConfig={firebase}>
+        <Suspense fallback="Conectando con  la app...">
+            <App />
+        </Suspense>
+    </FirebaseAppProvider>,
+    document.getElementById('root'),
+);
