a7c8edf648456073b38a24896e26e84819a5eaa1
Alammduran <alam_duran@hotmail.com>
2020-05-23 00:22:04 -0500

    Modifique el apartado de log in para hacer las pruebas de firebase (agregar usuario, iniciar sesión, cerrar sesión y mandar correo de verificación)

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,27 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useFirebaseApp, useUser } from 'reactfire';
 import { StyledLogin } from './styled';
+import 'firebase/auth';
 
 const LoginPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const firebase = useFirebaseApp();
+    const user = useUser();
+
+    // Crear usuario con correo y contraseña
+    const submit = e => {
+        e.preventDefault();
+        firebase.auth().createUserWithEmailAndPassword(email, password);
+        /*
+        Esta parte es la que manda el email de verificación
+        cuando se agregar el usuario pero no funciono porque el valor inicial
+        de la función es null (Ese es el error)
+        */
+        // user.signInWithEmailAndPassword()
+    };
+    // Iniciar sesión
+    const login = async e => {
+        e.preventDefault();
+        await firebase.auth().signInWithEmailAndPassword(email, password);
+    };
+    // Mandar email de verificación
+    const mensaje = async e => {
+        e.preventDefault();
+        await user.sendEmailVerification();
+    };
+    // Cerrar sesión
+    const logout = async e => {
+        e.preventDefault();
+        await firebase.auth().signOut();
+    };
+
     return (
         <StyledLogin>
             <div className="flexwrap">
                 <div>
                     <h1>Iniciar sesión</h1>
-                    <Form>
-                        <Form.Group controlId="formGroupEmail">
-                            <Form.Label>Email o nombre de ususario</Form.Label>
-                            <Form.Control type="email" />
-                        </Form.Group>
-                        <Form.Group controlId="formGroupPassword">
-                            <Form.Label>Contraseña</Form.Label>
-                            <Form.Control type="password" />
-                        </Form.Group>
-                        <Button className="boton" type="submit">
-                            Iniciar sesión
-                        </Button>
-                    </Form>
+                    {// Cuando el usuario no esta logeado
+                    !user && (
+                        <Form>
+                            <Form.Group controlId="formGroupEmail">
+                                <Form.Label>Email o nombre de ususario</Form.Label>
+                                <Form.Control
+                                    type="email"
+                                    controlid="email"
+                                    onChange={ev => setEmail(ev.target.value)}
+                                />
+                            </Form.Group>
+                            <Form.Group controlId="formGroupPassword">
+                                <Form.Label>Contraseña</Form.Label>
+                                <Form.Control
+                                    type="password"
+                                    controlid="password"
+                                    onChange={ev => setPassword(ev.target.value)}
+                                />
+                            </Form.Group>
+                            <Button className="boton" type="submit" onClick={submit}>
+                                Registrarse
+                            </Button>
+                            <Button className="boton" type="submit" onClick={login}>
+                                Iniciar sesión
+                            </Button>
+                        </Form>
+                    )}
+
+                    {// Cuando el usuario esta logeado
+                    user && (
+                        <Form>
+                            <Button className="boton" type="submit" onClick={logout}>
+                                Cerrar sesión
+                            </Button>
+                            <Button className="boton" type="submit" onClick={mensaje}>
+                                Mandar correo de verificación
+                            </Button>
+                        </Form>
+                    )}
                 </div>
                 <div>
                     <h1>Regístrate</h1>
@@ -47,6 +107,9 @@ const LoginPage = () => {
                                 Contraseña:
                                 <input type="password" name="name" />
                             </label>
+                            <Button className="boton" type="submit">
+                                Unirse
+                            </Button>
                             <Button className="boton" type="submit">
                                 Unirse
                             </Button>
