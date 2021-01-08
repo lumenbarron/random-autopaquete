f272330d5a3708b9c475b573849d870f0c2ac157
lumenbarron <lucemendez@gmail.com>
2021-01-08 11:45:36 -0600

    fixing bug date in sobrepesos

--- src/pages/send/origen.js
+++ src/pages/send/origen.js
@@ -127,31 +127,23 @@ export const OrigenComponent = ({ onSave, idGuiaGlobal }) => {
     let idGuia;
     let pickedDirection;
 
-    useEffect(() => {
-        let myHeaders = new Headers();
-        myHeaders.append('Authorization', tokenProd);
-        myHeaders.append('Content-Type', 'application/json');
-        let requestOptions = {
-            method: 'POST',
-            headers: myHeaders,
-            body: JSON.stringify({ shipping_id: 82696346 }),
-            redirect: 'follow',
-        };
-
-        // await Promise.all([
-        //     fetch('https://autopaquete.simplestcode.com/api/do-shipping-quote/redpack', requestOptions ),
-        //     fetch('https://autopaquete.simplestcode.com/api/do-shipping-quote/fedex', requestOptions)
-        // ])
-        // .then(([res1, res2]) =>
-        //     Promise.all([res1.json(), res2.json()]),
-        // )
-        const urlRequest = `https://autopaquete.simplestcode.com/api/tracking-shipping/redpack`;
-        console.log('url', urlRequest);
-        fetch(urlRequest, requestOptions)
-            .then(response => response.text())
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
-    }, []);
+    // useEffect(() => {
+    //     let myHeaders = new Headers();
+    //     myHeaders.append('Authorization', tokenProd);
+    //     myHeaders.append('Content-Type', 'application/json');
+    //     let requestOptions = {
+    //         method: 'POST',
+    //         headers: myHeaders,
+    //         body: JSON.stringify({ shipping_id: 82696346 }),
+    //         redirect: 'follow',
+    //     };
+    //     const urlRequest = `https://autopaquete.simplestcode.com/api/tracking-shipping/redpack`;
+    //     console.log('url', urlRequest);
+    //     fetch(urlRequest, requestOptions)
+    //         .then(response => response.text())
+    //         .then(result => console.log(result))
+    //         .catch(error => console.log('error', error));
+    // }, []);
 
     //Se busca los datos de envío (si hay algun envío efectuandose)
     useEffect(() => {
